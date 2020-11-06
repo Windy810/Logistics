6e5f47a0d26ebf525b7b5717bbb6d3d75bc69af1
root <root@MY-LOVELYLEGION.localdomain>
2020-11-06 14:39:27 +0800

    Add remote func

---
+++ cloudfunctions-aliyun/get/index.js
@@ -0,0 +1,7 @@
+'use strict';
+const db = uniCloud.database()
+exports.main = async (event, context) => {
+	const collection = db.collection('Logistics')
+	const res = await collection.limit(1).get()
+	return res
+};
